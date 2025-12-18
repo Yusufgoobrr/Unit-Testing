85d2d97a72c4b5c4dd6f03e68d32c2dd7bc05533
Yusufgoobrr <karalortgt@gmail.com>
2025-12-18 13:06:00 +0200

    Implemented a conventional way of Testing

--- src/test/java/org/amigoscode/CalculatorTest.java
+++ src/test/java/org/amigoscode/CalculatorTest.java
@@ -2,13 +2,27 @@ package org.amigoscode;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
+    private final Calculator underTest = new Calculator();
+
     @Test
-    public void canAddNumbers(){
-Calculator calculator = new Calculator();
-var result = calculator.add(3,3);
-assertEquals(6,result);
+    public void canAddNumbers() {
+        // 1) setup --> given
+
+        var number1 = 3;
+        var number2 = 3;
+
+        // 2) invocation --> when
+
+        var actual = underTest.add(number1, number2);
+        var expected = 6;
+
+        // 3) assertion --> then
+
+        assertThat(actual).isEqualTo(6);
     }
+
+
 }
